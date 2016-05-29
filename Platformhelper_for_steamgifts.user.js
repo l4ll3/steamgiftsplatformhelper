d0f8b757c2bf5764e5da19b9be838a1de903897a
Lalle <spam@lalle.se>
2016-05-29 23:20:56 +0200

    Use simple storage to save in local storage

--- Platformhelper_for_steamgifts.user.js
+++ Platformhelper_for_steamgifts.user.js
@@ -4,6 +4,7 @@
 // @version     0.1
 // @description Show and filter platform for SG++
 // @require     http://code.jquery.com/jquery.min.js
+// @require     https://cdn.jsdelivr.net/simplestorage/0.2.1/simpleStorage.min.js
 // @author      lalle
 // @include     http://www.steamgifts.com/*
 // @include     https://www.steamgifts.com/*
@@ -14,20 +15,55 @@
 
 $("head link:last")
 .after("<link rel=stylesheet type=text/css href=https://steamstore-a.akamaihd.net/public/css/v6/store.css>")
-.after("<style>span.platform_img {background-color: black; height:20px; display: inline-block; opacity: 0.80;} span.lalle__platform {position: absolute; top: 5px; right: 5px;} </style>");
+.after("<style>span.platform_img {background-color: black; height:20px; display: inline-block; opacity: 0.80;} div.lalle__platform {position: absolute; top: 5px; right: 5px;} </style>");
 
-function getHtml5Storage() {
+function appendPlatform(appendToNode, platformhtml, isImg) {
+  if (isImg) {
+   appendToNode.append("<div class=\"lalle__platform\">" + platformhtml + "</div>"); 
+  } else {
+    appendToNode.append("<div>" + platformhtml + "</div>"); 
+  }
+}
+
+function setPlatformHtml(steamUrl, appendToNode, filterNode, isImg)  {
   try {
-    if('localStorage' in window)  {
-      return window['localStorage'];
+    var platformhtml = "";
+    if (!steamUrl.length) {
+      return;
+    }
+    if (simpleStorage.canUse()) {
+      if (simpleStorage.hasKey(steamUrl))
+        {
+          platformhtml = simpleStorage.get(steamUrl);
+          appendPlatform(appendToNode, platformhtml, isImg);
+        }
     }
     else {
-      console.log("HTML5 storage does not exist");
-      return null;
+      console.warn("Local storage not supported, retrieving platforms will be slow!")
+    }
+    if (!platformhtml.length) {
+      GM_xmlhttpRequest({
+        method: "GET",
+        url: steamUrl,
+        onload: function(response) {
+          var responseN = $(response.responseText); 
+          var platform = responseN.find("div.game_area_purchase_platform");
+          if (platform.length) {
+            appendPlatform(appendToNode, platform.html(), isImg);
+            if (simpleStorage.canUse())  {
+              simpleStorage.set(steamUrl, platform.html(), {TTL: 2592000000});
+            }
+            if (filterNode != null)
+             filterPlatform(filterNode);
+          }
+        }
+      });    
+    }
+    else if (filterNode != null) {
+      filterPlatform(filterNode);
     }
   } catch (e) {
-    console.log("Failed get HTML5 storage");
-    return null;
+    console.error("Failed to set platform html on node: " + e)
   }
 }
 
@@ -35,42 +71,17 @@ function getHref(node, aClass) {
   try {
     return node.find(aClass).attr("href").trim();  
   } catch (e) {
-    console.log("get steam url error: " + e + " on node: " + node);
+    console.error("get steam url error: " + e + " on node: " + node);
     return "";
   }  
 }
 
 function addPlatformToGrid(gridTitle) {
-    var steamURL = getHref(gridTitle, "a.giveaway__icon");
-    if (!steamURL.length) {
+    if (gridTitle === null) 
       return;
-    }
-    var storage = getHtml5Storage();
-    var platformhtml = "";
-    if (storage != null) {
-      platformhtml = storage.getItem(steamURL);
-    }
-
-  if (platformhtml != null)  {
-    gridTitle.find("a:first").append("<span class=\"lalle__platform\">" + platformhtml + "</div>");
-    filterPlatform(gridTitle);
-  }
-  else {
-    GM_xmlhttpRequest({
-      method: "GET",
-      url: steamURL,
-      onload: function(response) {
-        var platform = $(response.responseText).find("div.game_area_purchase_platform");
-        if (platform.length) {
-          gridTitle.find("a:first").append("<span class=\"lalle__platform\">" + platform.html() + "</div>");
-          if (storage != null)  {
-            storage.setItem(steamURL, platform.html());
-          }
-          filterPlatform(gridTitle);
-        }
-      }
-    });
-  }
+    var steamURL = getHref(gridTitle, "a.giveaway__icon");
+    
+    setPlatformHtml(steamURL, gridTitle.find("a:first"), gridTitle, true);
 }
 
 function filterPlatform(node) {
@@ -98,32 +109,7 @@ function addPlatformToGiveawayPage() {
   wrappers.each(function() {
     var wrap = $(this);
     var steamURL = getHref(wrap, "a.global__image-outer-wrap");
-    if (!steamURL.length) {
-      return;
-    }
-    var storage = getHtml5Storage();
-    var platformhtml = "";
-    if (storage != null) {
-      platformhtml = storage.getItem(steamURL);
-    }
-    if (platformhtml != null)  {
-      wrap.find("div.featured__heading").append("<span>" + platformhtml + "</span>");
-    }
-    else {
-      GM_xmlhttpRequest({
-        method: "GET",
-        url: steamURL,
-        onload: function(response) {
-          var platform = $(response.responseText).find("div.game_area_purchase_platform");
-          if (platform.length) {
-            wrap.find("div.featured__heading").append("<span>" + platform.html() + "</span>");
-            if (storage != null)  {
-              storage.setItem(steamURL, platform.html());
-            }
-          }
-        }
-      });
-    }
+    setPlatformHtml(steamURL, wrap.find("div.featured__heading"),null, false)
   });
 }
 
@@ -132,36 +118,9 @@ function addPlatformGAs() {
   giveaways.each(function() {
     var header = $(this);
     var steamURL = getHref(header,"a.giveaway__icon");
+    setPlatformHtml(steamURL, header, header.closest(".giveaway__row-outer-wrap"), false)
     if (!steamURL.length) {
       return;
-    }
-
-    var storage = getHtml5Storage();
-
-    var platformhtml = "";
-    if (storage != null) {
-        platformhtml = storage.getItem(steamURL);
-      }
-
-    if (platformhtml != null) {
-      header.append("<span>" + platformhtml + "</div>");
-      filterPlatform(header.closest(".giveaway__row-outer-wrap"));
-    }
-    else {
-      GM_xmlhttpRequest({
-        method: "GET",
-        url: steamURL,
-        onload: function(response) {
-          var platform = $(response.responseText).find("div.game_area_purchase_platform");
-          if (platform.length) {
-            header.append("<span>" + platform.html() + "</div>");
-            filterPlatform(header.closest(".giveaway__row-outer-wrap"));
-            if (storage != null)  {
-              storage.setItem(steamURL, platform.html());
-            }
-          }
-        }
-      });
     }
   });
 }
