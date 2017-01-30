3a08e2b8f99b2b28f0c99550a4a4b1773374c0fe
l4ll3 <lallemailfrom+github.com@gmail.com>
2017-01-30 20:10:24 +0100

    Re-request if saved platform is undefined

--- Platformhelper_for_steamgifts.user.js
+++ Platformhelper_for_steamgifts.user.js
@@ -14,122 +14,124 @@
 // ==/UserScript==
 
 $("head link:last")
-.after("<link rel=stylesheet type=text/css href=https://steamstore-a.akamaihd.net/public/css/v6/store.css>")
-.after("<style>span.platform_img {background-color: black; height:20px; display: inline-block; opacity: 0.80;} div.lalle__platform {position: absolute; top: 5px; right: 5px;} </style>");
+    .after("<link rel=stylesheet type=text/css href=https://steamstore-a.akamaihd.net/public/css/v6/store.css>")
+    .after("<style>span.platform_img {background-color: black; height:20px; display: inline-block; opacity: 0.80;} div.lalle__platform {position: absolute; top: 5px; right: 5px;} </style>");
 
 function appendPlatform(appendToNode, platformhtml, isImg) {
-  if (isImg) {
-   appendToNode.append("<div class=\"lalle__platform\">" + platformhtml + "</div>"); 
-  } else {
-    appendToNode.append("<div>" + platformhtml + "</div>"); 
-  }
+    if (isImg) {
+        appendToNode.append("<div class=\"lalle__platform\">" + platformhtml + "</div>");
+    } else {
+        appendToNode.append("<div>" + platformhtml + "</div>");
+    }
 }
 
 function setPlatformHtml(steamUrl, appendToNode, filterNode, isImg)  {
-  try {
-    var platformhtml = "";
-    if (!steamUrl.length) {
-      return;
-    }
-    if (simpleStorage.canUse()) {
-      if (simpleStorage.hasKey(steamUrl))
-        {
-          platformhtml = simpleStorage.get(steamUrl);
-          appendPlatform(appendToNode, platformhtml, isImg);
+    try {
+        var platformhtml = "";
+        if (!steamUrl.length) {
+            return;
         }
-    }
-    else {
-      console.warn("Local storage not supported, retrieving platforms will be slow!")
-    }
-    if (!platformhtml.length) {
-      GM_xmlhttpRequest({
-        method: "GET",
-        url: steamUrl,
-        onload: function(response) {
-          var responseN = $(response.responseText); 
-          var platform = responseN.find("div.game_area_purchase_platform");
-          if (platform.length) {
-            appendPlatform(appendToNode, platform.html(), isImg);
-            if (simpleStorage.canUse())  {
-              simpleStorage.set(steamUrl, platform.html(), {TTL: 2592000000});
+
+        if (simpleStorage.canUse()) {
+            if (simpleStorage.hasKey(steamUrl))
+            {
+                platformhtml = simpleStorage.get(steamUrl);
+                appendPlatform(appendToNode, platformhtml, isImg);
             }
-            if (filterNode != null)
-             filterPlatform(filterNode);
-          }
         }
-      });    
+        else {
+            console.warn("Local storage not supported, retrieving platforms will be slow!");
+        }
+
+        if (platformhtml === undefined || !platformhtml.length) {
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: steamUrl,
+                onload: function(response) {
+                    var responseN = $(response.responseText);
+                    var platform = responseN.find("div.game_area_purchase_platform");
+                    if (platform.length) {
+                        appendPlatform(appendToNode, platform.html(), isImg);
+                        if (simpleStorage.canUse())  {
+                            simpleStorage.set(steamUrl, platform.html(), {TTL: 2592000000});
+                        }
+                        if (filterNode !== null)
+                            filterPlatform(filterNode);
+                    }
+                }
+            });
+        }
+        else if (filterNode !== null) {
+            filterPlatform(filterNode);
+        }
+    } catch (e) {
+        console.error("Failed to set platform html on node: " + e);
     }
-    else if (filterNode != null) {
-      filterPlatform(filterNode);
-    }
-  } catch (e) {
-    console.error("Failed to set platform html on node: " + e)
-  }
 }
 
 function getHref(node, aClass) {
-  try {
-    return node.find(aClass).attr("href").trim();  
-  } catch (e) {
-    console.error("get steam url error: " + e + " on node: " + node);
-    return "";
-  }  
+    try {
+        return node.find(aClass).attr("href").trim();
+    } catch (e) {
+        console.error("get steam url error: " + e + " on node: " + node);
+        return "";
+    }
 }
 
 function addPlatformToGrid(gridTitle) {
-    if (gridTitle === null) 
-      return;
+    if (gridTitle === null)
+        return;
     var steamURL = getHref(gridTitle, "a.giveaway__icon");
-    
+
     setPlatformHtml(steamURL, gridTitle.find("a:first"), gridTitle, true);
 }
 
 function filterPlatform(node) {
     if (window.location.href.indexOf("wishlist") != -1 ||
         window.location.href.indexOf("/user/") != -1 ) {
-      return;
+        return;
     }
     var hasPlatform = node.find("span.platform_img");
     var isLinux = node.find("span.linux");
     if (hasPlatform.length && !isLinux.length) {
-      node.hide();
+        node.hide();
     }
 }
 
 function addPlatformToSGPPGrid() {
-  var sgppGridTitles = $("div.SGPP__gridTile");
-  sgppGridTitles.each(function() {
-    var gridTitle = $(this);
-    addPlatformToGrid(gridTitle);
-  });  
+    var sgppGridTitles = $("div.SGPP__gridTile");
+    sgppGridTitles.each(function() {
+        var gridTitle = $(this);
+        addPlatformToGrid(gridTitle);
+    });
 }
 
 function addPlatformToGiveawayPage() {
-  var wrappers = $("div.featured__container").find("div.featured__inner-wrap");
-  wrappers.each(function() {
-    var wrap = $(this);
-    var steamURL = getHref(wrap, "a.global__image-outer-wrap");
-    setPlatformHtml(steamURL, wrap.find("div.featured__heading"),null, false)
-  });
+    var wrappers = $("div.featured__container").find("div.featured__inner-wrap");
+    wrappers.each(function() {
+        var wrap = $(this);
+        var steamURL = getHref(wrap, "a.global__image-outer-wrap");
+        setPlatformHtml(steamURL, wrap.find("div.featured__heading"),null, false);
+    });
 }
 
 function addPlatformGAs() {
-  var giveaways = $("h2.giveaway__heading");
-  giveaways.each(function() {
-    var header = $(this);
-    var steamURL = getHref(header,"a.giveaway__icon");
-    setPlatformHtml(steamURL, header, header.closest(".giveaway__row-outer-wrap"), false)
-    if (!steamURL.length) {
-      return;
-    }
-  });
+    var giveaways = $("h2.giveaway__heading");
+    giveaways.each(function() {
+        var header = $(this);
+        var steamURL = getHref(header,"a.giveaway__icon");
+        setPlatformHtml(steamURL, header, header.closest(".giveaway__row-outer-wrap"), false);
+        if (!steamURL.length) {
+            return;
+        }
+    });
 }
 
 
 //Add and filter
 $(document).ready(function() {
-  addPlatformToGiveawayPage();
-  addPlatformToSGPPGrid();
+    addPlatformToGiveawayPage();
+    addPlatformToSGPPGrid();
 });
 
 addPlatformGAs();
